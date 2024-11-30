99e90e8557e574fe5c5a130136e13f13ac2fdd1a
yeuda <yeudaba@ac.sce.ac.il>
2024-11-30 21:21:25 +0200

    new

--- Exercise_1.py
+++ Exercise_1.py
@@ -26,6 +26,7 @@ def IntegerNumber():
 
 
 
+
 """יצירת מערך שכל הספרות שלו מופיעות מספר הפעמים במערך השני"""
 def Array1(arrA,arrB):# [3,2,2]
     NewArray = []
@@ -40,6 +41,27 @@ def Array1(arrA,arrB):# [3,2,2]
 A = [6,7,8]
 B = [2,1,3]
 
-print(Array1(A,B))
+#print(Array1(A,B))
 
 
+
+empty_tuple = ()
+one_sized = (66.13, )
+ranged = tuple(range(2, 4))
+joined = one_sized + ranged
+
+#print(joined)
+
+x,y,z=3,4,5
+
+#print(z)
+
+x = [1,2,3]
+
+a,b,c=x
+
+f=lambda a,b: (a+b) # קיצור דרך במקום ליצור פונקציה שתעשה פעולה
+
+res1= f(a,b)
+
+print(res1)
