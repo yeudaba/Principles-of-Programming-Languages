98350913d11ec9b09d912a357c11597878c5f4b5
yeuda <yeudaba@ac.sce.ac.il>
2024-12-07 19:15:32 +0200

    add

--- ex1.py
+++ ex1.py
@@ -1,70 +1,142 @@
-def binary_search(arr, index_Number, num1, num2):
-    if num2 == 0:
-        return num2
-    temp = binary_search(arr, index_Number, num1, num2 - 1)
-    if (index_Number == arr[temp]):
-        return temp
+# yeuda
 
+print("Question 1")
+def binary_search(arr, num, S1, end):
+    """
+    A recursive function that performs a binary search.
+
+    Parameters:
+    arr (list): A sorted list to search through.
+    target (int): The value to search for.
+    start (int): The starting index of the search range.
+    end (int): The ending index of the search range.
+
+    Returns:
+    int: The index where the target value is found, or -1 if the value is not present.
+    """
+    # Base case: If the range is invalid
+    if S1 > end:
+        return -1
+
+    # Calculate the middle index
+    mid = (S1 + end) // 2
+
+    # Check if the target is at the middle index
+    if arr[mid] == num:
+        return mid
+
+    # If the target is smaller than the middle element, search the left half
+    elif num < arr[mid]:
+        return binary_search(arr, num, S1, mid - 1)
+
+    # If the target is larger than the middle element, search the right half
     else:
-        return arr[temp]
-
-
-arr = [1, 3, 5, 7, 9]
-num1 = 7
-num2 = 0
-num3 = 4
-
-print(binary_search(arr, num1, num2, num3))
-
-arr1 = [1, 2, 3]
-
-New_number = 2
-"""
-def generate_combinations(arr1,New_number):
-    if(New_number==0):
-        return list(arr1[New_number],New_number)
-    temp=generate_combinations(,New_number)
-    return temp.append(list(arr1[len(arr1),New_number]))
-
-print(generate_combinations(arr1,New_number))
-
-"""
-
-
-def generate_combinations(arr1, New_number):
-    if isinstance(arr1,list) and len(arr1) == 0:
-        return list[arr1[0], New_number]
-    temp = []
-    temp.append(generate_combinations(arr1.pop(-1), New_number))
-    if (arr1[len(arr1)] != New_number):
-        return temp.append(arr1[New_number-len(arr1)], New_number)
-    return temp
-
-
-print(generate_combinations(arr1, New_number))
-
-"""
-def generate_combinations(arr1, New_number):
-    # Base case: if the list is empty, return a new list with the number
-    if len(arr1) == 0:
-        return [[New_number]]
-
-    # Get the last element and rest of the list
-    last_element = arr1[-1]
-    rest = arr1[:-1]
-
-    # Recursive call to generate combinations from the rest of the list
-    temp = generate_combinations(rest, New_number)
-
-    # Add the new combination with the current last element and New_number
-    new_combinations = [comb + [last_element, New_number] for comb in temp]
-
-    return temp + new_combinations
+        return binary_search(arr, num, mid + 1, end)
 
 # Example usage
-arr1 = [1, 2, 3]
-New_number = 4
-print(generate_combinations(arr1, New_number))
+size=input("Enter the size of the array: ")
+arr = []
+for i in range(int(size)):
+    num6 = int(input("Enter your number for array: "))
+    arr.append(num6)
+index_start = int(input("Enter the starting index"))
+index_end = int(input("Enter the ending index"))
+
+num = int(input("Enter the number to search for: "))
+print(binary_search(arr, num, index_start, index_end))  # Output: 3
+
+print()
+
+##################################################################################################################################################
+##################################################################################################################################################
+print("Question 2")
+
+size2=input("Enter the size of the array: ")
+arr2 = []
+for i in range(int(size2)):
+    num11 = int(input("Enter your number for array: "))
+    arr2.append(num11)
+
+New_number2 = int(input("Enter the number to search for: "))
+def generate_combinations(arr2, New_number):# [1,2,3,4]  New_number=4
+    """
+       Generates all possible combinations of New_number elements from the given list.
+
+       Parameters:
+       lst (list): The list of elements to choose from.
+       New_number (int): The number of elements in each combination.
+
+       Returns:
+       list of lists: A list containing all possible combinations of New_number elements.
+       """
+    # Base case: If New_number is 0, there is exactly one combination - an empty list
+    if New_number == 0:
+        return [[]]
+
+    if not arr2 or New_number > len(arr2):
+        return []
+
+    first = generate_combinations(arr2[1:], New_number - 1)
+    first = [[arr2[0]] + combination for combination in first]
+
+    without_number = generate_combinations(arr2[1:], New_number)
+    return first + without_number
 
 
-"""
+print(generate_combinations(arr2, New_number2))
+
+print()
+##################################################################################################################################################
+##################################################################################################################################################
+
+print("Question 3")
+x = 5
+
+# Define a lambda function to multiply a number by 2
+f = lambda x: x * 2  # Multiply the input by 2
+def apply_twice(f, x):
+    """
+        Applies the given function `f` to the input `x` twice and returns the sum of the results.
+
+        Args:
+            f (function): A function that takes one argument and returns a value.
+            x (int): The input value to which the function will be applied.
+
+        Returns:
+            int: The sum of applying the function `f` twice to the input `x`.
+
+        Example:
+             f = lambda x: x * 2
+            apply_twice(f, 3)
+            12
+        """
+    sum = 0 # Initialize the sum to zero
+    for i in range(2):
+        # Add the result of applying the function `f` to `x` to the sum
+        sum = sum + f(x)# 1. sum = 0 + 6;    2. sum= 6+6;
+
+    return sum
+
+print(apply_twice(f,x))
+print()
+
+
+print("Question 4")
+
+def create_multiplier(n):
+    """
+      Creates a multiplier function that multiplies any given number by n.
+
+      Args:
+          n (int): The number to multiply by.
+
+      Returns:
+          function: A function that takes an input x and returns x multiplied by n.
+
+      """
+    return lambda x: x * n# n=2
+
+multiplier_by_3 = create_multiplier(3)
+print(multiplier_by_3(10))
+
+
